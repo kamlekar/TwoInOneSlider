d6546a14533324448d0b0fea6babfc35aba3e4c4
Mr_Green <kamlekar.venkatesh@gmail.com>
2015-02-03 10:42:59 +0530

    modified

--- twoinone.js
+++ twoinone.js
@@ -1,30 +1,59 @@
 var twoInOneSlider = (function () {
-    var one, two, separator,
+    var v = [], l = '1',
+    K = 'data-2in1-key', O = 'data-2in1-one', T = 'data-2in1-two', S = 'data-2in1-separator',
     init = function (main) {
-        one = main.children[0];
-        two = main.children[1];
-        separator = document.createElement('div');
-        separator.className = 'separator';
-        main.appendChild(separator);
-        separator.onmousedown = down;
+        if(main.children.length === 2){
+            setStyles();
+            var k = v.length, i = v[k] = {};
+            i.m = main;
+            main.setAttribute(K, k);
+            i.o = i.m.children[0];
+            i.t = i.m.children[1];
+            i.o.setAttribute(O,l);
+            i.t.setAttribute(T, l);
+            i.s = document.createElement('div');
+            i.s.setAttribute(S, l);
+            i.m.appendChild(i.s);
+            i.s.onmousedown = down;
+        }
     },
     down = function (e) {
-        main.onmousemove = move;
+        var k = getKey(e), i = v[k];
+        i.m.onmousemove = move;
         document.onmouseup = up;
     },
     move = function (e) {
-        var width = e.pageX - e.currentTarget.offsetLeft;
-        one.style.width = width + "px";
-        two.style.left = width + "px";
-        separator.style.left = width + "px";
+        var k = getKey(e), i = v[k];
+        var w = e.pageX - e.currentTarget.offsetLeft + "px";
+        i.o.style.width = w
+        i.t.style.left = w;
+        i.s.style.left = w;
     },
+    getKey = function(e){
+        var el = e.currentTarget;
+        if(el !== document && el.getAttribute(K)){
+            return el.getAttribute(K);
+        }
+        else{
+            el = e.target;
+            if(el.getAttribute(K)){
+                return el.getAttribute(K);
+            }
+            else{
+                return el.parentElement.getAttribute(K);
+            }
+        }
+    }
     up = function (e) {
-        main.onmousemove = null;
+        var k = getKey(e), i = v[k];
+        i.m.onmousemove = null;
         document.onmouseup = null;
+    },
+    setStyles = function(){
+
     }
     return {
         init: init
     };
 })();
-var main = document.querySelector('.main');
-twoInOneSlider.init(main);
+
